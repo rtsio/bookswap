fdb38288c97192920072e851a7362c5482d0bea4
rtsio <rostislav.tsiomenko@gmail.com>
2013-12-07 11:18:03 -0500

    more ui styling and logic

--- index.php
+++ index.php
@@ -32,8 +32,7 @@
             </div>
             <button type="submit" class="btn btn-success">Sign in</button>
             <?php else: ?>
-            Welcome back,  <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?>.
-            <a class="btn btn-alert" href="logout.php">Logout.</a>
+            <h3 class="text-muted">Welcome back,  <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?>.</h3>
             <?php endif; ?>
           </form>
        </div>
@@ -50,22 +49,30 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-   
-    Welcome back,  <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?>.<br />
-    Search: <input type="text">
-    Browse: <br />
+
+<form class="form-inline" role="form">
+  <div class="form-group">
+    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="ISBNs, titles, authors">
+  </div>
+  <button type="submit" class="btn btn-default">search</button>
+</form>
     <div>
+      <h4 class="text-muted">Browse: <br /></h4>
+      <table class="table">      
       <div class="btn-group-lg">
-        <a class="btn btn-success" href="browse.php?cat=biol">Biology</a>
-        <a class="btn btn-success" href="browse.php?cat=chem">Chemistry</a>
-        <a class="btn btn-success" href="browse.php?cat=econ">Economics</a>
-        <a class="btn btn-success" href="browse.php?cat=math">Mathematics</a><br>
-        <a class="btn btn-success" href="browse.php?cat=psyc">Psychology</a>
-        <a class="btn btn-success" href="browse.php?cat=clickers">Clickers</a>
-        <a class="btn btn-success" href="browse.php?cat=codes">Access codes</a>
-        <a class="btn btn-success" href="browse.php">More...</a>
+        <tr><td><a class="btn btn-success" href="browse.php?cat=biol">Biology</a></td>
+        <td><a class="btn btn-success" href="browse.php?cat=chem">Chemistry</a></td>
+        <td><a class="btn btn-success" href="browse.php?cat=econ">Economics</a></td>
+        <td><a class="btn btn-success" href="browse.php?cat=math">Mathematics</a><br></td></tr>
+        <tr><td><a class="btn btn-success" href="browse.php?cat=psyc">Psychology</a></td>
+        <td><a class="btn btn-success" href="browse.php?phil">Philosophy</a></td>
+        <td><a class="btn btn-success" href="browse.php?cat=clickers">Clickers</a></td>
+        <td><a class="btn btn-success" href="browse.php?cat=codes">Access codes</a></td></tr>
       </div>
-      <form action="browse.php" method="get">
+      </table>
+      <h4 class="text-muted">Or pick a major: </h4><br>
+      <form class="form" role="form" action="browse.php" method="get">
+        <div class="form-group">
         <select name="cat">
         <?php
         $majors = getAllMajorsArray();
@@ -74,7 +81,8 @@
         }
         ?>
         </select>
-        <input type="submit">
+        </div>
+        <button type="submit" class="btn btn-default" value="Free me from the tyranny of the textbook industry">
       </form>
     </div>
     <div>
