38d81af9c2e9aaee2fe5799ee78675e56d2f4bdd
rtsio <rostislav.tsiomenko@gmail.com>
2013-12-07 01:44:48 -0500

    more ui

--- index.php
+++ index.php
@@ -36,16 +36,12 @@
       </div>
     </div>
     <?php if (empty($_SESSION['user'])): ?>
-    Welcome to <?php echo UNIVERSITY_NAME ?>'s BookSwap! This is a book exchange
-    run by students, for students - cutting out the middlemen and making your education cheaper.
-    <div id="login">
-      <form action="php/login.php" method="POST">
-	<input type="text" name="user" placeholder="username" required>
-	<input type="password" name="pass" placeholder="password" required>
-	<input type="submit" value="Submit">
-      </form>
-    <br /><a href="register.html">Sign me up</a>
-    <br /><a href="reset.html">Forgot password</a>
+    <div class="jumbotron">
+      <div class="container">
+        <h1><?php echo UNIVERSITY_NAME; ?> Bookswap</h1>
+        <p>A modern bookexchange to empower students, cut out the middle man, and encourage reuse and community building. Built by students, for students, and limited to campus members.</p>
+        <p><a class="btn btn-primary btn-lg" role="button" href="register.html">Learn more &raquo;</a></p>        
+      </div>
     </div>
     <?php else: ?>
     Welcome back,  <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?>.<br />
@@ -56,45 +52,15 @@
         <a href="browse.php?cat=biol">Biology</a> | <a href="browse.php?cat=chem">Chemistry</a>
         <a href="browse.php?cat=econ">Economics</a> | <a href="browse.php?cat=math">Mathematics</a><br>
       </p>
-      <p>
-        <a href="browse.php?cat=psyc">Psychology</a> | <a href="browse.php?cat=clickers">Clickers</a>
-        <a href="browse.php?cat=codes">Access codes</a> | <a href="browse.php">More...</a>
-	<form action="browse.php" method="get">
-          <select name="cat">
-            <option value="afst">African studies</option>
-            <option value="agst">Aging studies </option>
-            <option value="amst">American studies </option>
-            <option value="ancs">Ancient studies</option>
-            <option value="anth">Anthropology </option>
-            <option value="arch">Archeology</option>
-            <option value="asia">Asian studies</option>
-            <option value="biol">Biology</option>
-            <option value="chem">Chemistry</option>
-            <option value="chin">Chinese</option>
-            <option value="cmpe">Computer engineering</option>
-            <option value="cmsc">Computer science</option>
-            <option value="econ">Economics</option>
-            <option value="engl">English</option>
-            <option value="enme">Engineering</option>
-            <option value="ges">Geography and Environmental studies</option>
-            <option value="grek">Greek</option>
-            <option value="happ">Health administration</option>
-            <option value="hist">History</option>
-            <option value="is">Information systems</option>
-            <option value="jpns">Japanese</option>
-            <option value="kore">Korean</option>
-            <option value="latn">Latin</option>
-            <option value="math">Mathematics</option>
-            <option value="mgmt">Management</option>
-            <option value="mll">Modern Languages and Linguistics</option>
-            <option value="musc">Music</option>
-            <option value="phil">Philosophy</option>
-            <option value="phys">Physics</option>
-            <option value="poli">Political sciences</option>
-            <option value="psyc">Psychology</option>
-            <option value="stat">Statistics</option>
-            <option value="other">Other</option>
-	   </select>
+    <form action="browse.php" method="get">
+      <select name="cat">
+      <?php
+      $majors = getAllMajorsArray();
+      foreach($majors as $code => $major){
+          echo "<option value='$code'>$major</option>";
+      }
+      ?>
+            </select>
           <input type="submit">
         </form>
       </p>
